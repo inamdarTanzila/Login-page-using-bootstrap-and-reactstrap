85c153808f93c93717863656498ab310a577420b
Tanzila <tanzila.inamdar@joshsoftware.com>
2020-03-31 23:45:20 +0530

    Login form using reactstrap and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input }
+from 'reactstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="login-app">
+      <h1>Login Page</h1>
+   
+
+    <FormGroup>
+<Input type = "email" placeholder="Username"></Input>
+    </FormGroup>
+
+    <FormGroup>
+    <Input type = "password" placeholder="Password"></Input>
+    </FormGroup>
+
+    <Button>Sign in</Button>
+<div className="text-center pt-3"> 
+Create an account
+</div>
+
+<div className="text-center">
+  <a href="/sign-up">Sign up</a>
+
+</div>
+    </Form>
   );
 }
 
